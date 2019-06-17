00710c83a8dd8374f99a4746e72c9262e4f1a8cd
Jarek Latek <jareklatek@gmail.com>
2019-06-17 21:12:32 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('siema!');
+const name = 'Jarek';
+const age = 35;
+
+console.log(`Nazywamm się ${name} i mam ${age} lat`);
+
+console.log('Cieszę się, że podglądasz moją pracę, ponieważ to znaczy, że się rozwijam. ;)');
