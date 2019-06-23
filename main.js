a0aff8d817db6326992bffe188b2828a1ecf2a16
Jarek Latek <jareklatek@gmail.com>
2019-06-23 11:57:26 +0200

    BEM modification and first DOM lesson

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ const age = 35;
 console.log(`Nazywamm się ${name} i mam ${age} lat`);
 
 console.log('Cieszę się, że podglądasz moją pracę, ponieważ to znaczy, że się rozwijam. ;)');
+
+const main__header = document.querySelector('.main__header--js');
+
+console.log(main__header.innerHTML);
+
+main__header.innerHTML = 'Pozdro JS';
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[0]);
